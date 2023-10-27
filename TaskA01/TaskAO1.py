c72706db07272aeb0a49e207caeb51eca1ce04dd
IgnBys <ignatbyshnev@gmail.com>
2023-10-27 16:57:36 +0200

    Edited TaskAO1

---
+++ TaskA01/TaskAO1.py
@@ -0,0 +1,46 @@
+import re
+
+
+def openFile(fileName,hamletArray = ['H','a', 'm', 'l', 'e', 't'], hamletFullWord = r'Hamlet'):
+    with open(fileName, "r", encoding="utf-8") as file:
+        file_contents = file.read()
+        repCounter = 0
+        line = ''
+        counter = 0
+        checkWord = ''
+
+        index = 0
+        length = len(file_contents)
+        while index < length:
+            character = file_contents[index]
+            line += character
+
+            if len(checkWord) < len(hamletFullWord):
+                if character == hamletArray[counter]:
+                    checkWord+=character
+                
+                    counter += 1
+
+                else:
+                    checkWord = ''
+                    counter = 0
+
+            if character=='\n':
+                if checkWord=='Hamlet':
+                    print(line)
+                line = ''
+                counter = 0
+                checkWord = ''
+
+            index+=1
+
+
+#
+print('---------------------SHAKESPEARE.EXP--------------------------------------')
+openFile('shakespeare.exp')
+print('----------------------SHAKESPEARE.EXP-----------------------------------')
+openFile('shakespeare.in')
+print('------------------------SIMPLE.IN------------------------------------')
+openFile('simple.in')
+print('-----------------------SIMPLE.EXP--------------------------------------')
+openFile('simple.exp')
